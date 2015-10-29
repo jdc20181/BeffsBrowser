85854950d9bda615f77db194a4f3f71d1f1e03bd
jdc20181 <jdc20181@gmail.com>
2015-10-29 12:55:01 -0400

    Create Favorites.vb
    This is the Sites.vb or favorites form create new form add a button to new form to call it.

---
+++ Favorites.vb
@@ -0,0 +1,81 @@
+//This is for the Favories or Sites file in BeffsBrowser you will add a button and add this dim window as new sites window.show//
+Imports System.Xml
+
+Public Class Sites
+
+    Private Sub frmSites_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        LoadList()
+    End Sub
+
+    Private Sub LoadList()
+        lvwWebSites.Items.Clear()
+
+        Dim rdrXML As New XmlTextReader(My.Application.Info.DirectoryPath & "\" & My.Settings.XmlFileName1)
+        rdrXML.MoveToContent()
+
+        Dim ElementName As String = ""
+        Dim NextItem As Boolean = True
+        Dim objListViewItem As ListViewItem = Nothing
+
+        Do While rdrXML.Read
+            If NextItem Then
+                objListViewItem = New ListViewItem
+                NextItem = False
+            End If
+            Select Case rdrXML.NodeType
+                Case XmlNodeType.Element
+                    ElementName = rdrXML.Name
+                Case XmlNodeType.Text
+                    If ElementName = "Name" Then
+                        objListViewItem.Text = rdrXML.Value
+                    End If
+                    If ElementName = "URL" Then
+                        objListViewItem.SubItems.Add(rdrXML.Value)
+                        lvwWebSites.Items.Add(objListViewItem)
+                        NextItem = True
+                    End If
+            End Select
+        Loop
+        rdrXML.Close()
+    End Sub
+
+    Private Sub btnOK_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnOK.Click
+        Dim wtrXML As New XmlTextWriter(My.Application.Info.DirectoryPath & "\" & My.Settings.XmlFileName1, System.Text.Encoding.UTF8)
+        With wtrXML
+            .Formatting = Formatting.Indented
+            .WriteStartDocument()
+            .WriteStartElement("WebSites")
+            Dim objListViewItem As New ListViewItem
+            For Each objListViewItem In lvwWebSites.Items
+                .WriteStartElement("WebSite")
+                .WriteElementString("Name", objListViewItem.Text)
+                .WriteElementString("URL", objListViewItem.SubItems(1).Text)
+                .WriteEndElement()
+            Next
+            .WriteEndElement()
+            .WriteEndDocument()
+            .Flush()
+            .Close()
+        End With
+
+        Me.Close()
+    End Sub
+
+    Private Sub btnAddSite_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAddSite.Click
+   Dim objListViewItem As New ListViewItem
+   objListViewItem.Text = txtWebSite.Text
+      objListViewItem.SubItems.Add(txtURL.Text)
+        lvwWebSites.Items.Add(objListViewItem)
+
+        txtWebSite.Text = ""
+        txtURL.Text = ""
+    End Sub
+
+    Private Sub btnRemoveSite_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnRemoveSite.Click
+        Dim indexes As ListView.SelectedIndexCollection = lvwWebSites.SelectedIndices
+        Dim index As Integer
+        For Each index In indexes
+            lvwWebSites.Items.RemoveAt(index)
+        Next
+    End Sub
+End Class 
