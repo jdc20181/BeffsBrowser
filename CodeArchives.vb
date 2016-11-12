055ef28f3b385810c003a5a683529bb8742e21ac
jdc20181 <jdc20181@gmail.com>
2016-11-12 09:35:48 -0500

    Update CodeArchives.vb

--- CodeArchives.vb
+++ CodeArchives.vb
@@ -1,5 +1,102 @@
 'Not offical project code but a directory of code snippets'
 
+'New Code for fullscreen needs to be edited.
+Public Class frmMainForm
+    Private Sub cmdCloseForm_Click(
+        ByVal sender As System.Object,
+        ByVal e As System.EventArgs) Handles cmdCloseForm.Click
+
+        Close()
+
+    End Sub
+    Private Sub cmdChange_Click(
+        ByVal sender As System.Object,
+        ByVal e As System.EventArgs) Handles cmdChange.Click
+
+        If cmdChange.Text = "Full" Then
+
+            cmdChange.Text = "Normal"
+            cmdMake.Text = "Normal"
+
+            FullScreen(chkTaskbar.Checked)
+
+            If chkTaskbar.Checked Then
+                Me.FullScreen(True)
+            End If
+        Else
+            cmdChange.Text = "Full"
+            cmdMake.Text = "Full"
+            NormalMode()
+        End If
+
+    End Sub
+    Private Sub cmdMsgBox_Click(
+        ByVal sender As System.Object,
+        ByVal e As System.EventArgs) Handles cmdMsgBox.Click
+        MessageBox.Show("Hello")
+    End Sub
+    Private Sub cmdShowChildForm_Click(
+        ByVal sender As System.Object,
+        ByVal e As System.EventArgs) Handles cmdShowChildForm.Click
+
+        Dim f As New frmChildForm
+        Dim TopMostSetting As Boolean = Me.TopMost
+
+        Try
+            Me.TopMost = False
+            f.ShowDialog()
+        Finally
+            f.Dispose()
+            Me.TopMost = TopMostSetting
+        End Try
+    End Sub
+    Public Sub MakeFullScreen()
+        Me.SetVisibleCore(False)
+        Me.FormBorderStyle = FormBorderStyle.None
+        Me.WindowState = FormWindowState.Maximized
+        Me.SetVisibleCore(True)
+    End Sub
+    Private Sub cmdMake_Click(
+        ByVal sender As System.Object,
+        ByVal e As System.EventArgs) Handles cmdMake.Click
+
+        If cmdMake.Text = "Full" Then
+            cmdMake.Text = "Normal"
+            cmdChange.Text = "Normal"
+            MakeFullScreen()
+        Else
+            cmdMake.Text = "Full"
+            cmdChange.Text = "Full"
+            NormalMode()
+        End If
+
+    End Sub
+    Private Sub cmdDetect_Click(sender As Object, e As EventArgs) Handles cmdDetect.Click
+        MessageBox.Show(Me.WindowState.ToString)
+    End Sub
+    Private Sub frmMainForm_StyleChanged(
+        sender As Object,
+        e As EventArgs) Handles Me.StyleChanged
+
+        ListBox1.Items.Add(Me.WindowState.ToString)
+        ListBox1.SelectedIndex = ListBox1.Items.Count - 1
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        NormalMode()
+
+    End Sub
+End Class
+
+
+
+
+
+
+
+
+______________________________
 'Attention this code is archived!
 'Enter fullscreen
   ToolStripButton5.Visible = False
