aee6716fe3d8c623b489e4a5763bbd013f14264a
jdc20181 <jdc20181@gmail.com>
2019-04-27 09:21:47 -0400

    Create favicons.vb

---
+++ CodeSnippets/favicons.vb
@@ -0,0 +1,13 @@
+    'Do not use TODO test this code see if it work better
+    'Private Sub SurroundingSub()
+    '    Dim w As HttpWebRequest = CType(HttpWebRequest.Create("http://stackoverflow.com/favicon.ico"), HttpWebRequest)
+    '    w.AllowAutoRedirect = True
+    '    Dim r As HttpWebResponse = CType(w.GetResponse(), HttpWebResponse)
+    '    Dim ico As System.Drawing.Image
+
+    '    Using s As Stream = r.GetResponseStream()
+    '        ico = System.Drawing.Image.FromStream(s)
+    '    End Using
+
+    '    ico.Save("favicon.ico")
+    'End Sub
